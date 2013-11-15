57e482d82eec6a2ded58ff82e00a9c62b7a74874
Jonathan Stoler <jrstoler@Jonathans-MacBook-Pro.local>
2013-11-14 23:09:33 -0800

    fixed multiple bugs in boot.php
    - now, it's okay if the database has already been created (but
    Chartridge will still automatically create it if necessary)
    - boot.php will no longer affect the existing structure of a database,
    but it will still set up databases for use with Chartridge
    - fixed a bug that could produce a PDOException

--- boot.php
+++ boot.php
@@ -6,76 +6,76 @@
 	if($con){
 		$db = mysql_select_db(c::get('db.name'), $con);
 		if(!$db){
-			mysql_query('CREATE DATABASE ' . c::get('db.prefix').c::get('db.name'));
+			mysql_query('CREATE DATABASE `' . c::get('db.prefix').c::get('db.name') . '`');
 			mysql_close($con);
-			db::connect();
-			db::query(
-				"CREATE TABLE `bonuses` (
-				  `session` text NOT NULL,
-				  `game` text NOT NULL,
-				  `name` text NOT NULL,
-				  `time` datetime NOT NULL
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-				CREATE TABLE `checkpoints` (
-				  `session` text NOT NULL,
-				  `game` text NOT NULL,
-				  `name` text NOT NULL,
-				  `time` datetime NOT NULL
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-				CREATE TABLE `data` (
-				  `session` text NOT NULL,
-				  `game` text NOT NULL,
-				  `name` text NOT NULL,
-				  `value` text NOT NULL
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-				CREATE TABLE `games` (
-				  `name` text NOT NULL,
-				  `created` datetime NOT NULL,
-				  `id` text NOT NULL,
-				  `checkpoints` text NOT NULL,
-				  `bonuses` text NOT NULL,
-				  `disable_checkpoint_unit` int(11) NOT NULL DEFAULT '0',
-				  `disable_bonus_unit` int(11) NOT NULL DEFAULT '0',
-				  `disable_score_unit` int(11) NOT NULL DEFAULT '0',
-				  `disable_increment_unit` int(11) NOT NULL DEFAULT '0',
-				  `disable_data_unit` int(11) NOT NULL DEFAULT '0'
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-				CREATE TABLE `increments` (
-				  `session` text NOT NULL,
-				  `game` text NOT NULL,
-				  `name` text NOT NULL,
-				  `value` int(11) NOT NULL
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-				CREATE TABLE `players` (
-				  `id` text NOT NULL,
-				  `game` text NOT NULL
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-				CREATE TABLE `scores` (
-				  `session` text NOT NULL,
-				  `game` text NOT NULL,
-				  `time` datetime NOT NULL,
-				  `mode` text NOT NULL,
-				  `score` float NOT NULL
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-				CREATE TABLE `sessions` (
-				  `id` text NOT NULL,
-				  `player` text NOT NULL,
-				  `game` text NOT NULL,
-				  `start` datetime NOT NULL,
-				  `ip` text NOT NULL,
-				  `location` text NOT NULL
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1;"
-			);
-
-			header('Location: ' . uri::current()->baseurl() . DS . c::get('chartridge.root'));
 		}
+		db::connect();
+		db::execute(
+			"CREATE TABLE IF NOT EXISTS `bonuses` (
+			  `session` text NOT NULL,
+			  `game` text NOT NULL,
+			  `name` text NOT NULL,
+			  `time` datetime NOT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+			CREATE TABLE IF NOT EXISTS `checkpoints` (
+			  `session` text NOT NULL,
+			  `game` text NOT NULL,
+			  `name` text NOT NULL,
+			  `time` datetime NOT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+			CREATE TABLE IF NOT EXISTS `data` (
+			  `session` text NOT NULL,
+			  `game` text NOT NULL,
+			  `name` text NOT NULL,
+			  `value` text NOT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+			CREATE TABLE IF NOT EXISTS `games` (
+			  `name` text NOT NULL,
+			  `created` datetime NOT NULL,
+			  `id` text NOT NULL,
+			  `checkpoints` text NOT NULL,
+			  `bonuses` text NOT NULL,
+			  `disable_checkpoint_unit` int(11) NOT NULL DEFAULT '0',
+			  `disable_bonus_unit` int(11) NOT NULL DEFAULT '0',
+			  `disable_score_unit` int(11) NOT NULL DEFAULT '0',
+			  `disable_increment_unit` int(11) NOT NULL DEFAULT '0',
+			  `disable_data_unit` int(11) NOT NULL DEFAULT '0'
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+			CREATE TABLE IF NOT EXISTS `increments` (
+			  `session` text NOT NULL,
+			  `game` text NOT NULL,
+			  `name` text NOT NULL,
+			  `value` int(11) NOT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+			CREATE TABLE IF NOT EXISTS `players` (
+			  `id` text NOT NULL,
+			  `game` text NOT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+			CREATE TABLE IF NOT EXISTS `scores` (
+			  `session` text NOT NULL,
+			  `game` text NOT NULL,
+			  `time` datetime NOT NULL,
+			  `mode` text NOT NULL,
+			  `score` float NOT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+			CREATE TABLE IF NOT EXISTS `sessions` (
+			  `id` text NOT NULL,
+			  `player` text NOT NULL,
+			  `game` text NOT NULL,
+			  `start` datetime NOT NULL,
+			  `ip` text NOT NULL,
+			  `location` text NOT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1"
+		);
+
+		header('Location: ' . uri::current()->baseurl() . DS . c::get('chartridge.root'));
 	}
 	
 	header('Location: ' . uri::current()->baseurl() . DS . c::get('chartridge.root'));
